02ab7497518a66459d689a566ee6cfeba8cf7946
SmilingCreeper <155292827+SmilingCreeper@users.noreply.github.com>
2024-06-03 20:55:30 +0800

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,31 +1,9 @@
-<meta charset="UTF-8">
-<?php
-$username = 'root';
-$password = 'root';
-$ip = 'smilingcreeper.github.io';
-$database = 'database';
-$conn = new mysqli($ip,$username,$password,$database);
-$logname = $_GET['name'];
-$password1 = $_GET['password1'];
-$password2 = $_GET['password2'];
- 
-$sql = "select * from table_1 where username = '$logname';";
-$res = mysqli_query($conn,$sql);
- 
-if($res->num_rows > 0){
-	echo '用户已存在,3秒后跳转,请重新输入。';
-	header('Refresh:3,https://smilingcreeper.github.io/home/reg.html');
+$db_host="192.168.0.9";
+$db_name="SmilingCreeper";
+$db_pwd="20130203Edward:;
+$link=mysqli_connect($db_host,$db_name,$db_pwd);
+if($link){
+  echo "success";
 }else{
-	if($password1 != $password2){
-		echo'密码不一致。';
-	}else{
-		$sql = "insert into table_1 value('$logname','$password1');";
-		if(mysqli_query($conn,$sql)){
-			echo'注册成功,3秒后返回登录页面。';
-			header('Refresh:3,http://smilingcreeper.github.io/home/login.html');
-		}else{
-			echo'注册失败。';
-		}
-	}
+  echo "fail";
 }
-?>
